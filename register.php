57e95b9265fcb54255cfb064f7da149581071bd5
Marwan HARNAFI <mharnafi@enc-sio.fr>
2022-11-21 17:10:42 +0100

    Changement

--- register.php
+++ register.php
@@ -8,33 +8,50 @@
         $nom_prof = $_POST['nom_prof'];
         $prenom_prof = $_POST['prenom_prof'];
         $adresse_mail = $_POST['adresse_mail'];
-        $mot_de_passe = sha1($_POST['mot_de_passe']);
-
-        if(!empty($id_numen) && !empty($nom_prof) && !empty($prenom_prof) && !empty($adresse_mail) && !empty($mot_de_passe)){
-
-
-            include "./ConnexionMySQL.php";
-            global $db;
-
-            $c = $db->prepare("SELECT ADRESSE_MAIL FROM professeurs WHERE ADRESSE_MAIL = ?");
-            $c->execute([$adresse_mail]);
-            $result = $c->rowCount();
+        $num_telephone = $_POST['num_telephone'];
+        $mot_de_passe = $_POST['mot_de_passe'];
+        $c_mot_de_passe = $_POST['c_mot_de_passe'];
 
 
-            if($result == 0){
-                $q = $db->prepare("INSERT INTO professeurs(ID_NUMEN, NOM_PROF, PRENOM_PROF, ADRESSE_MAIL, MOT_DE_PASSE) VALUES(?, ?, ?, ?, ?)");
-                $q->bindParam($id_numen, $nom_prof, $prenom_prof, $adresse_mail, $mot_de_passe);
-                $q->execute([$id_numen, $nom_prof, $prenom_prof, $adresse_mail, $mot_de_passe]);  
-                echo '<script>alert("Le compte a été créée")</script>';
+        if(!empty($id_numen) && !empty($nom_prof) && !empty($prenom_prof) && !empty($adresse_mail) && !empty($num_telephone) && !empty($mot_de_passe) && !empty($c_mot_de_passe)){
+
+            if($mot_de_passe == $c_mot_de_passe){
+                include "./ConnexionMySQL.php";
+                global $db;
+
+                $c = $db->prepare("SELECT ADRESSE_MAIL FROM professeurs WHERE ADRESSE_MAIL = ?");
+                $c->execute([$adresse_mail]);
+                $result = $c->rowCount();
+
+
+                $options = ['cost' => 12];
+                $hashpass = password_hash($mot_de_passe, PASSWORD_BCRYPT, $options);
+
+
+                if($result == 0){
+                    $q = $db->prepare("INSERT INTO professeurs(ID_NUMEN, NOM_PROF, PRENOM_PROF, ADRESSE_MAIL, NUM_TELEPHONE, MOT_DE_PASSE) VALUES(?, ?, ?, ?, ?, ?)");
+                    $q->bindParam($id_numen, $nom_prof, $prenom_prof, $adresse_mail, $num_telephone, $hashpass);
+                    $q->execute([$id_numen, $nom_prof, $prenom_prof, $adresse_mail, $num_telephone, $hashpass]);  
+                    $message = "Le compte a été créée avec succès.";
+                    echo "<script type='text/javascript'>alert('$message');</script>";
+                }
+                else{
+                    $message = "Un compte avec cette adresse mail existe déjà.";
+                    echo "<script type='text/javascript'>alert('$message');</script>";
+                }
             }
-            else{
-                echo '<script>alert("Un compte avec cette adresse mail existe déjà.")</script>';
+
+            else
+            {
+                $message = "Les deux mots de passe sont incorrect.";
+                echo "<script type='text/javascript'>alert('$message');</script>";
             }
             
         }
         
         else{
-            echo '<script>alert("Les champs sont vides")</script>';
+            $message = "Les champs sont vides.";
+            echo "<script type='text/javascript'>alert('$message');</script>";
         }
     
 
@@ -55,7 +72,7 @@
     <div class="wrapper">
         <a href="https://www.enc-bessieres.org/"><img src="./logo_enc.png"></a>
     <h1>S'enregistrer</h1>
-    <form method="POST" action="register.php">
+    <form method="POST" action="">
         ID Numen :
         <input name="id_numen" id="id_numen" type="text" placeholder="ex: ABC123ABDTRDA">
         <br>
@@ -68,8 +85,14 @@
         Mail : 
         <input name="adresse_mail" id="adresse_mail" type="text" placeholder="exemple@enc-bessieres.org">
         <br>
+        N° Téléphone :
+        <input name="num_telephone" id="num_telephone" type="text" placeholder="ex: 01 23 45 67 89">
+        <br>
         Mot de passe :
         <input name="mot_de_passe" id="mot_de_passe" type="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
+        <br>
+        Confirmation du mot de passe :
+        <input name="c_mot_de_passe" id="c_mot_de_passe" type="password" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
         <br>
         <input type="submit" id="soumettre" name="soumettre" value="S'enregistrer" />
     </form>
@@ -77,4 +100,3 @@
     </div>
 </body>
 </html>
-
