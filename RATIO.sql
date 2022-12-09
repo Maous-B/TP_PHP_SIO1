167722dd6ba902ec551c729e082c65429fef3296
Maous-B <79797065+Maous-B@users.noreply.github.com>
2022-12-09 15:43:19 +0100

    Update RATIO.sql

--- RATIO.sql
+++ RATIO.sql
@@ -1,2 +1,4 @@
-#INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('SIO', 'Service Informatique aux Organisations', '2022/2024')
-INSERT INTO CANDIDAT()
+INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('SIO', 'Service Informatique aux Organisations', '2022/2024')
+INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('NDRC', 'Négociation Digitalisation de la Relation Client', '2022/2024')
+INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('CG', 'Comptabilité et Gestion', '2022/2024')
+
