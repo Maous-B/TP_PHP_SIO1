d99955763d0e85cca5ee8384c25d76f4141022fe
Maous-B <79797065+Maous-B@users.noreply.github.com>
2022-12-17 18:10:02 +0100

    Update liaisons_eleves_filieres.sql

--- liaisons_eleves_filieres.sql
+++ liaisons_eleves_filieres.sql
@@ -1,4 +1,5 @@
 SELECT c.NOM_CANDIDAT, c.PRENOM_CANDIDAT, f.LIBELLE_BTS
 FROM candidat c
 INNER JOIN filiere_bts f
-ON c.fk_id_filiere_bts = f.id_bts
+ON c.fk_id_bts = f.id_bts
+WHERE f.code_bts= 'SIO'
