f363620b004fb6a8979dcd13e6e1888f997d1a0b
Maous-B <79797065+Maous-B@users.noreply.github.com>
2022-12-23 15:24:17 +0100

    Delete RATIO.sql

--- RATIO.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('SIO', 'Service Informatique aux Organisations', '2022/2024');
-INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('NDRC', 'Négociation Digitalisation de la Relation Client', '2022/2024');
-INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('CG', 'Comptabilité et Gestion', '2022/2024');
-
-INSERT INTO candidat(NOM_CANDIDAT, PRENOM_CANDIDAT, fk_id_bts) VALUES('HARNAFI', 'Marwan', '1');
-INSERT INTO candidat(NOM_CANDIDAT, PRENOM_CANDIDAT, fk_id_bts) VALUES('LEGAUD', 'Rayan', '1');
-INSERT INTO candidat(NOM_CANDIDAT, PRENOM_CANDIDAT, fk_id_bts) VALUES('BOUANANE', 'Fethi', '2');
