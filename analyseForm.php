f5904b7a3988e99d2068653ed1b9f30d1cb23527
Maous-B <79797065+Maous-B@users.noreply.github.com>
2022-12-30 22:41:26 +0100

    Update analyseForm.php

--- analyseForm.php
+++ analyseForm.php
@@ -6,6 +6,24 @@
         include './ConnexionMySQL.php';
         global $db;
         //var_dump($_POST);
+        
+        $r = "SELECT p.ID_PROF
+        FROM professeurs p
+        WHERE p.ADRESSE_MAIL = :a_m";
+
+        $q = $db->prepare($r);
+
+        $q->bindParam(':a_m', $_SESSION['add_mail']);
+
+        $q->execute();
+
+        $donnees = $q->fetch();
+        
+        foreach($donnees as $ind => $value){
+            global $id_prof;
+            $id_prof = $value;
+        }
+        
         $index = 0;
         $id_notes = array_combine($_POST['idEleve'], $_POST['noteEpreuve']);
         $commentaires = $_POST['commentaire'];
@@ -13,7 +31,7 @@
             //echo $id." est l'id de l'élève et la note est de ".$notes."<br>";
             $REQUETE_SQL_FINAL_ENVOI_NOTES = "INSERT INTO 
             epreuve_ccf(DATE_EPREUVE, HEURE_CONVOCATION, DUREE_EPREUVE, NOTE_OBTENU, COEFFICIENT, COMMENTAIRE_EVALUATION, fk_clef_eleve, fk_id_ccf, fk_id_bts) 
-            VALUES(:date_e, :heure_conv, :duree_e, :note_o, :coef, :com_eval, :fk_c_e, :fk_i_c, :fk_i_b);";
+            VALUES(:date_e, :heure_conv, :duree_e, :note_o, :coef, :com_eval, :fk_c_e, :fk_i_p, :fk_i_c, :fk_i_b);";
             $q_final = $db->prepare($REQUETE_SQL_FINAL_ENVOI_NOTES);
             $q_final->bindParam(':date_e', $_POST['dateEpreuve'][0], PDO::PARAM_STR);
             $q_final->bindParam(':heure_conv', $_POST['heure_convocation'], PDO::PARAM_STR);
@@ -22,6 +40,7 @@
             $q_final->bindParam(':coef', $_POST['coefficient'], PDO::PARAM_INT);
             $q_final->bindParam(':com_eval', $commentaires[$index], PDO::PARAM_STR);
             $q_final->bindParam(':fk_c_e', $id, PDO::PARAM_INT);
+            $q_final->bindParam(':fk_i_p', $id_prof, PDO::PARAM_INT);
             $q_final->bindParam(':fk_i_c', $_POST['epreuve_ccf'][0], PDO::PARAM_INT);
             $q_final->bindParam(':fk_i_b', $_POST['idBts'][0], PDO::PARAM_INT);
             $q_final->execute();
