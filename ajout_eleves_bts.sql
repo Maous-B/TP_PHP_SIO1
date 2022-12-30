a4d96a3a13ba165a21a1b11541bc4e2e2e3836b0
Maous-B <79797065+Maous-B@users.noreply.github.com>
2022-12-30 13:59:45 +0100

    Update ajout_eleves_bts.sql

--- ajout_eleves_bts.sql
+++ ajout_eleves_bts.sql
@@ -2,12 +2,13 @@ INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('SIO', 'S
 INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('NDRC', 'Négociation Digitalisation de la Relation Client', '2022/2024');
 INSERT INTO filiere_bts(CODE_BTS, LIBELLE_BTS, ANNEE_CONCERNEE) VALUES('CG', 'Comptabilité et Gestion', '2022/2024');
 
-INSERT INTO candidat(fk_id_bts, NUMERO_ELEVE, NOM_CANDIDAT, PRENOM_CANDIDAT) VALUES(1, 'ADQ1', 'HARNAFI', 'Marwan');
-INSERT INTO candidat(fk_id_bts, NUMERO_ELEVE, NOM_CANDIDAT, PRENOM_CANDIDAT) VALUES(2, 'AKS2', 'ROBERT', 'Timothée');
-INSERT INTO candidat(fk_id_bts, NUMERO_ELEVE, NOM_CANDIDAT, PRENOM_CANDIDAT) VALUES(2, 'BDF6', 'BEAUDOUX', 'Camille');
-INSERT INTO candidat(fk_id_bts, NUMERO_ELEVE, NOM_CANDIDAT, PRENOM_CANDIDAT) VALUES(3, 'AZS66', 'DEBIEVE', 'Hugo');
-INSERT INTO candidat(fk_id_bts, NUMERO_ELEVE, NOM_CANDIDAT, PRENOM_CANDIDAT) VALUES(2, 'ASW31', 'JOUARY', 'Antoine');
-INSERT INTO candidat(fk_id_bts, NUMERO_ELEVE, NOM_CANDIDAT, PRENOM_CANDIDAT) VALUES(1, 'AGH13', 'BOUANANE', 'Fethi');
+INSERT INTO `candidat` (`CLEF_ELEVE`, `NUMERO_ELEVE`, `NOM_CANDIDAT`, `PRENOM_CANDIDAT`, `fk_id_bts`) VALUES
+	(1, 'ADQ1', 'HARNAFI', 'Marwan', 1),
+	(2, 'AKS2', 'ROBERT', 'Timothée', 2),
+	(3, 'BDF6', 'BEAUDOUX', 'Camille', 2),
+	(4, 'AZS66', 'DEBIEVE', 'Hugo', 3),
+	(5, 'ASW31', 'JOUARY', 'Antoine', 2),
+	(6, 'AGH13', 'BOUANANE', 'Fethi', 1);
 
 INSERT INTO ccf(CODE_CCF, LIBELLE_CCF, fk_id_bts) VALUES('E6', 'Cybersécurité des organisations', 1);
 INSERT INTO ccf(CODE_CCF, LIBELLE_CCF, fk_id_bts) VALUES('E4', 'Développement', 1);
