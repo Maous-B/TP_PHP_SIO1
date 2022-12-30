e28e66bf37632fabb105dca738e9ea9f4b20d2b2
Maous-B <79797065+Maous-B@users.noreply.github.com>
2022-12-30 23:27:51 +0100

    Update analyseForm.php

--- analyseForm.php
+++ analyseForm.php
@@ -1,12 +1,14 @@
 <?php
-    if(isset($_POST['soumettre_final'])){
 
-        extract($_POST);
+    session_start();
+
+    if(isset($_POST['soumettre_final'])){
 
         include './ConnexionMySQL.php';
         global $db;
-        //var_dump($_POST);
-        
+
+        extract($_POST);
+
         $r = "SELECT p.ID_PROF
         FROM professeurs p
         WHERE p.ADRESSE_MAIL = :a_m";
@@ -23,14 +25,15 @@
             global $id_prof;
             $id_prof = $value;
         }
-        
+
+        //var_dump($_POST);
         $index = 0;
         $id_notes = array_combine($_POST['idEleve'], $_POST['noteEpreuve']);
         $commentaires = $_POST['commentaire'];
         foreach($id_notes as $id => $notes){
             //echo $id." est l'id de l'élève et la note est de ".$notes."<br>";
             $REQUETE_SQL_FINAL_ENVOI_NOTES = "INSERT INTO 
-            epreuve_ccf(DATE_EPREUVE, HEURE_CONVOCATION, DUREE_EPREUVE, NOTE_OBTENU, COEFFICIENT, COMMENTAIRE_EVALUATION, fk_clef_eleve, fk_id_ccf, fk_id_bts) 
+            epreuve_ccf(DATE_EPREUVE, HEURE_CONVOCATION, DUREE_EPREUVE, NOTE_OBTENU, COEFFICIENT, COMMENTAIRE_EVALUATION, fk_clef_eleve, fk_id_prof, fk_id_ccf, fk_id_bts) 
             VALUES(:date_e, :heure_conv, :duree_e, :note_o, :coef, :com_eval, :fk_c_e, :fk_i_p, :fk_i_c, :fk_i_b);";
             $q_final = $db->prepare($REQUETE_SQL_FINAL_ENVOI_NOTES);
             $q_final->bindParam(':date_e', $_POST['dateEpreuve'][0], PDO::PARAM_STR);
